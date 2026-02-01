3c562c0faea5a1d9876c13e9d13d4277429eb13a
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-02-01 17:43:10 -0300

    feat: Validating user data

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,8 +1,26 @@
 import { Request, Response } from "express"
+import { UserRole } from "@prisma/client"
+import { z } from "zod"
 
 class UsersController {
   async create(request: Request, response: Response){
-    response.json({ message: "User created successfully!" })
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: "Name is required"}),
+      email: z.string().trim().email({ message: "E-mail is invalid"}).toLowerCase(),
+      password: z.string().min(6, { message: "Password must be at least 6 characters long"}),
+      role: z.enum([UserRole.employee, UserRole.manager]).default(UserRole.employee)
+    })
+
+    // const { name, email, password, role } = bodySchema.parse(request.body)
+    const result = bodySchema.safeParse(request.body)
+
+    if (!result.success) {
+      return response.status(400).json({ errors: result.error.flatten() })
+    }
+
+    const { name, email, password, role } = result.data
+
+    response.json({ name, email, password, role })
   }
 }
 
