2e4f992ca0920c2224680dc2221aae97724efbfe
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-02-01 17:27:16 -0300

    feat: Creating routes and controller for Users

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,18 @@ import express from "express"
 import cors from "cors"
 
 import { errorHandling } from "./middlewares/error-handling"
+import { routes } from "./routes"
 
 const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) => {
-
-  res.send("Hello, Refund API!")
-})
-
 app.use(errorHandling)
+app.use(routes)
+
+// app.get("/", (req, res) => {
+
+//   res.send("Hello, Refund API!")
+// })
 
 export { app }
