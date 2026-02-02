277f70ef1c402f8a69b97048ca0b49836525412b
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-02-02 09:16:48 -0300

    feat: Refunds route and controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,10 +4,15 @@ import { usersRoutes } from "./users-routes";
 
 import { sessionsRoutes } from "./sessions-routes";
 
+import { refundsRoutes } from "./refunds-routes";
+
 const routes = Router()
 
 //Public routes
 routes.use("/users", usersRoutes)
 routes.use("/sessions", sessionsRoutes)
 
-export { routes}
+//Private routes
+routes.use("/refunds", refundsRoutes)
+
+export { routes }
