347a7ed91b2e4b37ba1a5c4b7cebac3848fd0330
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-02-03 09:42:39 -0300

    feat: Validating user data

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express';
+import { z } from "zod"
+
+//Defining list os possible categories using zod enum
+const CategoriesEnum = z.enum([
+  "food",
+  "others",
+  "services",
+  "transport",
+  "accomodation"
+])
 
 class RefundsController{
   async create (request: Request, response: Response){
+    const bodySchema = z.object({
+      name: z.string().trim().min(1, { message: "Name is a required field" }),
+      category: CategoriesEnum,
+      amount: z.number().positive({ message: "Amount must be a positive number" }),
+      filename: z.string().min(20, { message: "Filename is required" })
+    })
+
+    const { name, category, amount, filename } = bodySchema.parse(request.body)
+
     response.json({ message: "Refund created successfully!"})
   }
 } 
