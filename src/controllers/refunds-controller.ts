2c4fd79aa9086f2eea506ddc5541dcfd1d75b1e1
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-02-03 10:17:28 -0300

    feat: Inserting the refund into the database. Fixing some misspelling in prisma.schema

--- src/controllers/refunds-controller.ts
+++ src/controllers/refunds-controller.ts
@@ -1,4 +1,6 @@
+import { AppError } from '@/utils/AppError';
 import { Request, Response } from 'express';
+import { prisma } from '@/database/prisma';
 import { z } from "zod"
 
 //Defining list os possible categories using zod enum
@@ -7,7 +9,7 @@ const CategoriesEnum = z.enum([
   "others",
   "services",
   "transport",
-  "accomodation"
+  "accommodation"
 ])
 
 class RefundsController{
@@ -16,12 +18,26 @@ class RefundsController{
       name: z.string().trim().min(1, { message: "Name is a required field" }),
       category: CategoriesEnum,
       amount: z.number().positive({ message: "Amount must be a positive number" }),
-      filename: z.string().min(20, { message: "Filename is required" })
+      filename: z.string().min(20, { message: "Filename is required and must be at least 20 characters long" })
     })
 
     const { name, category, amount, filename } = bodySchema.parse(request.body)
 
-    response.json({ message: "Refund created successfully!"})
+    if (!request.user?.id) {
+      throw new AppError("User not authorized", 401)
+    }
+
+    const refund = await prisma.refunds.create({
+      data: {
+        name,
+        category,
+        amount,
+        filename,
+        userId: request.user?.id
+      }
+    })
+
+    response.status(201).json(refund)
   }
 } 
 
